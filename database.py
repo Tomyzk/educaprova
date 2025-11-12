41662a07af9d2cb703646b4642d6a6a8d9309231
Henrique Machado <henriquegamadf21@gmail.com>
2025-11-12 16:05:51 -0300

    Inclui novamente o arquivo database.py

---
+++ database.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+def conectar():
+    """Cria e retorna uma conexão com o banco de dados MySQL."""
+    try:
+        conexao = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1234",
+            database="educaprova"
+        )
+        if conexao.is_connected():
+            print("Conectado ao banco de dados MySQL.")
+        return conexao
+    except Error as e:
+        print("Erro ao conectar ao MySQL:", e)
+        return None
